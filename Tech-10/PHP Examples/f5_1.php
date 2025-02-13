7fb4fb04fd4560394e2b2731492c1a1cca98bae8
mishrapriyanshu0103 <mishrapriyanshu0103>
2025-02-13 21:14:03 +0530

    Acedemic Showcase

--- Tech-10/PHP Examples/f5_1.php
+++ Tech-10/PHP Examples/f5_1.php
@@ -1,9 +1,9 @@
 <?php
-$nm=$_POST["nm"];
-$cl=$_POST["cl"]
-echo "<table border='1' bgcolor=$cl>";
-for($i=1;$i<=$nm;$i++){
-echo "<tr><td height='20' width='100'></tr></td>";
+$nm = $_POST["nm"];
+$cl = $_POST["cl"];
+echo "<table border='1' bgcolor='$cl'>";
+for ($i = 1; $i <= $nm; $i++) {
+    echo "<tr><td height='20' width='100'></td></tr>";
 }
 echo "</table>";
 ?>
